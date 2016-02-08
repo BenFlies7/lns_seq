b1c6a8e48f0a34663b1905b1a81368fff14e2b30
benflies <ben.flies.001@student.uni.lu>
2016-02-08 14:54:02 +0100

    Initial commit

---
+++ Scripts_Thesis/coverage_boxplot.py
@@ -0,0 +1,33 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pybedtools
+import time
+import urllib
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_coverage(bam_file,bed_file):
+    """
+    Compute the coverage from an input BAM file,
+    by only taking into consideration the regions
+    provided in the BED file.
+    """
+    almnt = pybedtools.BedTool(bam_file)
+    regions = pybedtools.BedTool(bed_file)
+    print "Calculating coverage over regions ..."
+    sys.stdout.flush()
+    t0 = time.time()
+    coverage_result = almnt.coverage(regions).sort()
+    print coverage_result
+    coverage_array = np.array([i[-4] for i in coverage_result], dtype=float)
+    print coverage_array
+    t1 = time.time()
+    print "completed in %.2fs" % (t1-t0)
+    sys.stdout.flush()
+    plt.boxplot(coverage_array)
+    plt.show()
+    #print coverage_array
+
+get_coverage("/mnt/files-bioseq/bioseq-temporary/ben/Test/assembly.bam", "00100-1407755742_Regions.bed")
